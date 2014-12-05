24ec1ed4ccc2663ef0533a83effaae980851a18a
Bruce <185178371@qq.com>
2014-12-05 15:14:59 +0800

    use Parse Blog

--- js/blog.js
+++ js/blog.js
@@ -2,13 +2,19 @@ $(function() {
 
     Parse.$ = jQuery;
 
-    // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("Deo1ECOrS6K4PTYNzK5shswZGsGKkuh7N5F4YDkJ", "1dDstVTMqi18grcBzivfTHXx4HWBQe8Q2jijwQuu");
 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({model:Blog});
+    var blogs = new Blogs();
+
+    blogs.fetch({
+    success: function(blogs) {
+        console.log(blogs);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
+});
 
 });
