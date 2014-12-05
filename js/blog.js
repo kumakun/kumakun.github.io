ee46f886186932fd114ba078219118d260f47d44
Bruce <185178371@qq.com>
2014-12-05 15:57:16 +0800

    nothing

--- js/blog.js
+++ js/blog.js
@@ -4,28 +4,28 @@ $(function() {
 
     Parse.initialize("Deo1ECOrS6K4PTYNzK5shswZGsGKkuh7N5F4YDkJ", "1dDstVTMqi18grcBzivfTHXx4HWBQe8Q2jijwQuu");
 
-    var Blog = Parse.Object.extend("Blog");
-    var Blogs = Parse.Collection.extend({model:Blog});
-    var blogs = new Blogs();
-
-    var BlogsView =  Parse.View.extend({
-    template: Handlebars.compile($('#blogs-tpl').html()),
-    render: function(){
+    var Blog = Parse.Object.extend("Blog"),
+    Blogs = Parse.Collection.extend({
+      model: Blog
+    }),
+    BlogsView = Parse.View.extend({
+      template: Handlebars.compile($('#blogs-tpl').html()),
+      render: function() {
         var collection = { blog: this.collection.toJSON() };
         this.$el.html(this.template(collection));
-    }});
+      }
+    });
+    blogs = new Blogs();
 
     blogs.fetch({
-    success: function(blogs) {
+      success: function(blogs) {
         var blogsView = new BlogsView({ collection: blogs });
         blogsView.render();
         $('.main-container').html(blogsView.el);
-    },
-    error: function(blogs, error) {
+      },
+      error: function(blogs, error) {
         console.log(error);
-    }
-
-
-});
+      }
+    });
 
 });
